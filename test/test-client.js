533e644147d046b9b3d7c712d9d206fa6cfba3f7
bcoe <bencoe@gmail.com>
2014-05-14 01:24:46 -0400

    added in tests, refactored somewhat.

--- test/test-client.js
+++ test/test-client.js
@@ -16,6 +16,56 @@ describe('thumbnail', function() {
       },
     });
 
-    client.thumbnail('/foo/bar.jpg', {});
+    client.thumbnail('/foo/bar.jpg', []);
   });
+
+  it("should allow prefix to be overridden by opts", function(done) {
+    var client = new Client({
+      sqs: {
+        sendMessage: function(sqsObject) {
+          var obj = JSON.parse(
+            sqsObject.MessageBody
+          )
+          assert.equal(obj.prefix, '/banana');
+          done();
+        },
+        endpoint: {} // adhere to SQS contract.
+      },
+    });
+
+    client.thumbnail('/foo/bar.jpg', [], {prefix: '/banana'});
+  });
+
+  it('should execute callback when it is the third parameter', function(done) {
+    var client = new Client({
+      sqs: {
+        sendMessage: function(sqsObject, cb) {
+          cb(null, 'success');
+        },
+        endpoint: {} // adhere to SQS contract.
+      },
+    });
+
+    client.thumbnail('/foo/bar.jpg', [], function(err, message) {
+      assert.equal(message, 'success');
+      done();
+    });
+  });
+
+  it('should execute callback when it is the fourth parameter', function(done) {
+    var client = new Client({
+      sqs: {
+        sendMessage: function(sqsObject, cb) {
+          cb(null, 'success');
+        },
+        endpoint: {} // adhere to SQS contract.
+      },
+    });
+
+    client.thumbnail('/foo/bar.jpg', [], {}, function(err, message) {
+      assert.equal(message, 'success');
+      done();
+    });
+  });
+
 });
--- test/test-thumbnailer.js
+++ test/test-thumbnailer.js
@@ -2,9 +2,9 @@ var assert = require('assert'),
   sinon = require('sinon'),
   Thumbnailer = require('../lib/thumbnailer').Thumbnailer;
 
-describe("#Thumbnailer", function() {
+describe("Thumbnailer", function() {
 
-  describe("#createConversionPath", function() {
+  describe("createConversionPath", function() {
 
     it("tmp.file() executed with appropriate parameters", function() {
       // we mock tmp.file() which is called
@@ -29,8 +29,8 @@ describe("#Thumbnailer", function() {
 
   });
 
-  describe('#execCommand', function() {
-    
+  describe('execCommand', function() {
+
     beforeEach(function() {
       // create a thumbnailer with some of the
       // internals stubbed out.
@@ -48,9 +48,9 @@ describe("#Thumbnailer", function() {
       this.thumbnailer.execCommand.restore();
     });
 
-    describe('#bounded', function() {
+    describe('bounded', function() {
 
-      it('appropriate convert command generated if no quality set', function() {
+      it('generates appropriate convert command generated if no quality set', function() {
         var convertCommand = 'convert "/tmp/111.png[0]" -thumbnail 96X96 /tmp/222.jpg';
 
         // We don't actually want to execute the
@@ -70,7 +70,7 @@ describe("#Thumbnailer", function() {
         mock.verify();
       });
 
-      it('appropriate convert command generated if quality set', function() {
+      it('generates appropriate convert command generated if quality set', function() {
         var convertCommand = 'convert "/tmp/111.png[0]" -thumbnail 96X96 -quality 75 /tmp/222.jpg';
 
         // We don't actually want to execute the
@@ -93,6 +93,55 @@ describe("#Thumbnailer", function() {
 
     });
 
+    describe('manual', function() {
+      it('generates a custom convert command, and executes it with appropriate parameters', function() {
+        var convertCommand = "convert '/tmp/111.png' '/tmp/333.png' /tmp/222.jpg";
+
+        // We don't actually want to execute the
+        // convert command.
+        var mock = sinon.mock(this.thumbnailer)
+          .expects('execCommand')
+          .once()
+          .withArgs(convertCommand);
+
+        this.thumbnailer.execute({
+          strategy: "%(command)s '%(localPaths[0])s' '%(localPaths[1])s' %(convertedPath)s"
+        }, ['/tmp/111.png', '/tmp/333.png']);
+
+        mock.verify();
+      });
+    });
+
+  });
+
+  describe('_guessStrategy', function() {
+    it('it returns strategy name, if strategy exists', function() {
+      var thumbnailer = new Thumbnailer({
+        strategy: 'bounded'
+      });
+
+      assert.equal(thumbnailer._guessStrategy(), 'bounded');
+    });
+
+    it('executes onComplete with error, if strategy not found', function(done) {
+      var thumbnailer = new Thumbnailer({
+        strategy: 'unknown',
+        onComplete: function(err) {
+          assert.equal(err.message, 'could not find strategy unknown');
+          done();
+        }
+      });
+
+      thumbnailer._guessStrategy();
+    });
+
+    it("returns 'manual' as strategy, if %(command)s is present", function() {
+      var thumbnailer = new Thumbnailer({
+        strategy: '%(command)s foo -bar'
+      });
+
+      assert.equal(thumbnailer._guessStrategy(), 'manual');
+    });
   });
 
 });
